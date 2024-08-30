47a43ee9fe978c4a12d51575060b8e418df555cf
Rodrigo Pereira <145811429+rodrigoPS01@users.noreply.github.com>
2024-08-30 13:55:53 -0300

    script.js

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-const formulario = document.querySelector('#formulario');
-const valorInput = document.querySelector('#valor');
-const servico = document.querySelector('#servico');
-const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario')
+const valorInput = document.querySelector('#valor')
+const servico = document.querySelector('#servico')
+const resultado = document.querySelector('#resultado')
 
 const calcularGorjeta = (valor, qualidade) => {
-    let percentual;
+    let percentual
 
     switch (qualidade) {
         case 'bom':
             percentual = 0.15; 
-            break;
+            break
         case 'regular':
             percentual = 0.10; 
-            break;
+            break
         case 'ruim':
             percentual = 0.05; 
-            break;
+            break
         default:
             percentual = 0;
     }
 
-    return valor * percentual;
-};
+    return valor * percentual
+}
 
 const exibirResultado = (valor, qualidade) => {
-    const gorjeta = calcularGorjeta(valor, qualidade);
+    const gorjeta = calcularGorjeta(valor, qualidade)
     resultado.textContent = `
     O total da sua refeição é: R$ ${(valor + gorjeta).toFixed(2)}
     e a gorjeta é: R$ ${gorjeta.toFixed(2)}
-    `;
-};
+    `
+}
 
 formulario.addEventListener('submit', (event) => {
-    event.preventDefault();
+    event.preventDefault()
     
-    const valor = parseFloat(valorInput.value);
-    const qualidade = servico.value;
+    const valor = parseFloat(valorInput.value)
+    const qualidade = servico.value
     
-    exibirResultado(valor, qualidade);
+    exibirResultado(valor, qualidade)
 });
